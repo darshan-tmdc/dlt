95e00536ffde76af8b3db24f2f425b9c5c7e8b66
Darshan Ajmera <darshan.ajmera@tmdc.io>
2025-06-11 17:56:05 +0530

    change in example pipeline

--- examples/elastic-search/es-pipeline.py
+++ examples/elastic-search/es-pipeline.py
@@ -1,10 +1,11 @@
 import dlt
-@dlt.resource(write_disposition="merge")
+@dlt.resource(write_disposition="merge", primary_key='id')
 def get_users_data(updated_at=dlt.sources.incremental("updated_at", initial_value="2025-01-01T00:00:00Z")):
     # Sample data source
     data = [
         {"id": 3, "name": "Alice", "updated_at": "2025-05-31 14:50:00"},
-        {"id": 4, "name": "Bob", "updated_at": "2025-05-31 14:55:00"}
+        {"id": 4, "name": "Bob", "updated_at": "2025-06-01 14:55:00"},
+        {"id": 5, "name": "Charlie", "updated_at": "2025-06-02 15:00:00", "is_active": True}
     ]
     # Filter data based on the incremental 'updated_at' value
     for row in data:
@@ -13,10 +14,13 @@ def get_users_data(updated_at=dlt.sources.incremental("updated_at", initial_valu
 if __name__ == "__main__":
     pipeline = dlt.pipeline(
         pipeline_name="elasticsearch_pipeline",
-        destination=dlt.destinations.elasticsearch(
-            host="http://localhost:9200",
-            username="elastic",
-            password="changeme"
+        destination=dlt.destinations.elasticsearch(credentials={
+            "protocol": "http",
+            "host": "localhost",
+            "port": 9200,
+            "username": "elastic",
+            "password": "changeme"
+        }
         )
     )
 
